be8046576b0ed35747d1707be5646a51ac9c1c17
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-06-13 20:50:53 +0800

    Initial Commit

---
+++ UnderTheWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnderTheWeather
+//
+//  Created by Francis Jemuel Bergonia on 13/06/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
